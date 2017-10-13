976f84e03c8b2d088adc70d2f30d38af46ab0d04
zack <zack@MacbookProOfZack.local>
2017-10-13 14:10:38 +0800

    text

---
+++ ZackProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZackProject
+//
+//  Created by 姬彦 on 2017/10/13.
+//  Copyright © 2017年 zack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZackProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZackProject
+//
+//  Created by 姬彦 on 2017/10/13.
+//  Copyright © 2017年 zack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
